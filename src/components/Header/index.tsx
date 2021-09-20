6f749edb830c5705eac9a0fd1857e55cf023c73e
Gabriel Santana <gabrielsantana77@hotmail.com>
2021-09-19 23:26:53 -0300

    componentizando

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
 
 export function Header() {
     return (
-        <header>
+        <Container>
+					<Content>
             <img src={logoImg} alt="dt money" />
             <button type="button">
                 Nova transação
             </button>
-        </header>
+					</Content>
+        </Container>
         
     )
 }
